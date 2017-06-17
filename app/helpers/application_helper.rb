3c3c273fb1252e55447c7752f0aa3cd28445f27a
Burke Ramsey <birk5437@gmail.com>
2017-06-17 17:54:10 -0400

    display shift dates in correct time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def money_format(value)
     "$#{'%.02f' % value.to_d}"
   end
+
+  #https://github.com/scottwater/detect_timezone_rails
+  def format_time(time, timezone="America/New_York", strftime_format="%A %B %e, %Y %R%P")
+    time.to_time.in_time_zone(timezone).strftime(strftime_format)
+  end
 end
